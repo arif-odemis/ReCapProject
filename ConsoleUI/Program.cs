0d2ec273335d32f130c4c1757ac646fbb2d56c85
arif-odemis <arif.odemis@hotmail.com>
2022-10-29 01:21:21 +0300

    8.Ders ödev

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +10,9 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
-			CarManager carManager = new CarManager(new InMemoryCarDal());
-			foreach (var car in carManager.GetAll())
-			{
-				Console.WriteLine(car.CarId);
-			}
-		
+			CarManager carManager = new CarManager(new EfCarDal());
+			carManager.Add(new Car { CarId = 5, BrandId = 5, Description = "X", ColorId = 1, DailyPrice = 0, ModelYear = 2008 }); //Araba eklenmicek istenilen şartlara uymuyor.
 		}
+
 	}
 }
